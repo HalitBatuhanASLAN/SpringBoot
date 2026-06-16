d657c968bb083b02f13051df2f404c999f3e6155
HalitBatuhanASLAN <halitbatuhanaslan@gmail.com>
2026-06-16 16:53:51 +0300

    restcomplete test completed

--- productrestapi/src/test/java/com/example/springweb/ProductrestapiApplicationTests.java
+++ productrestapi/src/test/java/com/example/springweb/ProductrestapiApplicationTests.java
@@ -2,12 +2,52 @@ package com.example.springweb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.springweb.entities.Product;
 
 @SpringBootTest
 class ProductrestapiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testGetProduct(){
+		RestTemplate restTemplate = new RestTemplate();
+		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/1",Product.class);
+	
+		assertNotNull(product);
+		assertEquals("asfasffasfaffasfdaf",product.getName());
+		
+	
+	}
+	
+	@Test
+	void testCreateProduct()
+	{
+		RestTemplate restTemplate = new RestTemplate();
+		
+		Product product = new Product();
+		product.setName("Samsung Mpbile");
+		product.setDescription("Good");
+		product.setPrice(100);
+		Product newProduct = restTemplate.getForObject("http://localhost:8080/productapi/products/",Product.class);
+	
+		assertNotNull(newProduct);
+		assertNotNull(newProduct.getId());
+		
+		assertEquals("Samsung Mpbile",newProduct.getName());
+		
 	}
 
+	
+	@Test
+	void testUpdateProduct(){
+		RestTemplate restTemplate = new RestTemplate();
+		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/1",Product.class);
+		product.setPrice(100000);
+		
+		restTemplate.put("http://localhost:8080/productapi/products/1", product);
+	
+	}
 }
